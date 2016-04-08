43a0edb2baacf4329317d83745e47c42da33bc8f
Jonathan Fuqua <jfuqua390@gmail.com>
2016-04-08 11:00:18 -0500

    User can submit new run, and navigate site

---
+++ app/new/controller.js
@@ -0,0 +1,37 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  submitForm() {
+    const attributes = {
+      time: this.time,
+      date: this.date,
+      notes: this.notes,
+    };
+    fetch(`https://tiny-tn.herokuapp.com/collections/runs-jf`, {
+      method: `POST`,
+      headers: {
+        Accept: `application/JSON`,
+        'Content-type': `application/JSON`,
+      },
+      body: JSON.stringify(attributes),
+    }).then((r) => r.json())
+    .then((newRun) => {
+      this.clearForm();
+      this.addNewRun(newRun);
+    });
+  },
+
+  cancelForm() {
+
+  },
+
+  clearForm() {
+    this.set(`time`, ``);
+    this.set(`date`, ``);
+    this.set(`notes`, ``);
+  },
+
+  addNewRun(run) {
+    this.set(`model`, [run, ...this.model]);
+  },
+});
